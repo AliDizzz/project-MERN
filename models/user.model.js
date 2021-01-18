c5c6655b54fb74fa88d6e029d4ed73a7c2b425e4
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-18 11:23:19 +0100

    user.model created commit

---
+++ models/user.model.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+
+const userShema = new mongoose.Shema(
+    {
+        pseudo: {
+            type: String,
+            required: true,
+            minLength: 3,
+            maxLength: 55,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            validate:  [isEmail],
+            lowercase: true,
+            trim: true,
+        },
+        password: {
+            typr: String,
+            required: true,
+            max: 1024,
+            minLength: 6
+        },
+        bio: {
+            type: String,
+            max: 1024,
+        },
+        followers: {
+            type: [String]
+        },
+        following: {
+            type: [String]
+        },
+        likes: {
+            type: [String]
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
