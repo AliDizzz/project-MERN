2cac2b09fe3aff2068206dfccc563af99cd727f6
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-20 14:34:02 +0100

    cookie-parser commit

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 // ROUTER
 app.use('/api/user', userRoutes);
