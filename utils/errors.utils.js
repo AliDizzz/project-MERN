9bb682af128fc5b98bcd61ea892e01cb5a970285
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-21 12:16:18 +0100

    upload.controller commit

--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -2,31 +2,43 @@ module.exports.signUpErrors = (err) => {
     let errors = { pseudo: '', email: '', password: '' }
 
     if (err.message.includes('pseudo'))
-    errors.pseudo = "Pseudo incorrect ou déjà pris";
+        errors.pseudo = "Pseudo incorrect ou déjà pris";
 
     if (err.message.includes('email'))
-    errors.email = 'Email incorrect';
+        errors.email = 'Email incorrect';
 
     if (err.password.includes('password'))
-    errors.password = 'Le mot de passe doit faire 6 caractères minimum';
+        errors.password = 'Le mot de passe doit faire 6 caractères minimum';
 
     if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("pseudo"))
-    errors.email = 'Ce pseudo est déjà pris';
+        errors.email = 'Ce pseudo est déjà pris';
 
     if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("email"))
-    errors.email = 'Cet email est déjà enregistré';
+        errors.email = 'Cet email est déjà enregistré';
 
     return errors
 };
 
 module.exports.signInErrors = (err) => {
-    let errors = { email: '', password: ''}
+    let errors = { email: '', password: '' }
 
     if (err.message.includes("email")) // si err.message inclue " email " renvoie moi ça :
-    errors.email = "Email inconnu";
+        errors.email = "Email inconnu";
 
     if (err.password.includes("password"))
-    errors.password = "Le mot de passe ne correspond pas"
+        errors.password = "Le mot de passe ne correspond pas"
+
+    return errors
+}
+
+module.exports.uploadErrors = (err) => {
+    let errors = { format: '', maxSize: "" };
+
+    if (err.message.includes('invalid file'))
+        errors.format = "Format incompatabile";
+
+    if (err.message.includes('max size'))
+        errors.maxSize = "Le fichier dépasse 500ko";
 
     return errors
 }
