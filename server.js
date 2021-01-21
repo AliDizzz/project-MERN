2b8777cdb49248f2fc3209abe6912ce37d45c7dd
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-21 11:38:38 +0100

    post.controller commit

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 const postRoutes = require('./routes/post.routes');
-require('dotenv').config({path: './config/.env'});
+require('dotenv').config({ path: './config/.env' });
 require('./config/db');
-const {checkUser, requireAuth} = require('./middleware/auth.middleware');
+const { checkUser, requireAuth } = require('./middleware/auth.middleware');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
 // jwt
@@ -18,7 +18,7 @@ app.get('/jwtid', requireAuth, (req, res) => {
     res.status(200).send(res.locals.user._id)
 });
 
-// ROUTER
+// routes
 app.use('/api/user', userRoutes);
 app.use('/api/post', postRoutes);
 
