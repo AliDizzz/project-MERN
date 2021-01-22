c370ddea70588bfac7e35c9f2a6d9897ed0132cf
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-22 11:51:28 +0100

    log page part 2

--- client/src/components/Log/index.js
+++ client/src/components/Log/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SignInForm from './SignInForm';
+import SignUpForm from './SignUpForm';
 
-const index = () => {
+const Log = () => {
+    const [signUpModal, setSignUpModal] = useState(true);
+    const [signInModal, setSignInModal] = useState(false);
+
+    const handleModals = (e) => {
+        if (e.target.id === "register") {
+            setSignInModal(false);
+            setSignUpModal(true);
+        } else if (e.target.id === "login") {
+            setSignUpModal(false);
+            setSignInModal(true);
+        }
+    }
+
     return (
         <div className="connection-form">
             <div className="form-container">
                 <ul>
-                    <li></li>
+                    <li onClick={handleModals} id="register" >S'inscrire</li>
+                    <li onClick={handleModals} id="login" >Se connecter</li>
                 </ul>
+                {signUpModal && <SignUpForm />}
+                {signInModal && <SignInForm />}
             </div>
         </div>
     );
 };
 
-export default index;
+export default Log;
