15efca128aa842d46758a606fc362db5af2c70af
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-22 11:36:25 +0100

    log page part 1

---
+++ client/src/components/Log/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const index = () => {
+    return (
+        <div className="connection-form">
+            <div className="form-container">
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default index;
