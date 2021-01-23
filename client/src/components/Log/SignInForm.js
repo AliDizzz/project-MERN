e2defb469d852b0cf72cbb5bc5cf0f1323d22b04
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-23 10:53:57 +0100

    input email

--- client/src/components/Log/SignInForm.js
+++ client/src/components/Log/SignInForm.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const SignInForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = (e) => {
+
+    }
     return (
-        <div>
-            Connecter
-        </div>
+        <form action="" onSubmit={handleLogin} id="sign-up-form" >
+            <label htmlFor="email"> Email </label>
+            <br/>
+            <input 
+            type="text" 
+            name="email" 
+            id="email" 
+            onChange={(e) => setEmail(e.target.value)} 
+            value={email}
+            />
+            <input 
+            type="submit" 
+            value="Se connecter" 
+            />
+        </form>
     );
 };
 
