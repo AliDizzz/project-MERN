35d0594f741340a44bce51e842ea7823c5ec039c
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-25 10:01:35 +0100

    navbar part 2

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useContext } from "react";
+
+import { NavLink } from "react-router-dom";
+import { UidContext } from "./AppContext";
 
 const Navbar = () => {
+    const uid = useContext(UidContext);
+    
+
     return (
-        <div>
-            Navbar
-        </div>
+        <nav>
+            <div className="nav-container">
+                <div className="logo">
+                    <NavLink exact to="/">
+                        <div className="logo">
+                            <img src="./img/icon.png" alt="icon" />
+                            <h3>Raccoont</h3>
+                        </div>
+                    </NavLink>
+                </div>
+                {/* si uid existe fait moi ça */}
+                {uid ? (
+                    <ul>
+                        <li></li>
+                        <li className="welcome">
+                            <NavLink exact to="/profil">
+                                <h5>Bienvenue 'valeur dynamique'</h5>
+                            </NavLink>
+                        </li>
+                        Logout 
+                    </ul>
+                ) : (
+                        <ul>
+                            <li></li>
+                            <li>
+                                <NavLink exact to="/profil">
+                                    <img src="./img/icons/login.svg" alt="login" />
+                                </NavLink>
+                            </li>
+                        </ul>
+                    )}
+            </div>
+        </nav>
     );
 };
 
