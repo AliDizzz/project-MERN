41b0406c367afcb7acfeadfcac798fe095b9073d
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-28 13:42:26 +0100

    trends

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,6 +4,7 @@ import usersReducer from './users.reducer';
 import postReducer from './post.reducer';
 import errorReducer from './error.reducer';
 import allPostsReducer from './allPosts.reducer';
+import trendingReducer from './trending.reducer';
 
 
 export default combineReducers({
@@ -11,5 +12,6 @@ export default combineReducers({
     usersReducer,
     postReducer,
     errorReducer,
-    allPostsReducer
+    allPostsReducer,
+    trendingReducer
 });
